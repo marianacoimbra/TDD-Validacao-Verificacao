e41dc6e4e126990342b778b9bdbd2fa5bf4f3ac2
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-07 19:51:46 -0300

    feat: add welcome message to app class

--- gerador-nota-fiscal/src/main/java/com/gerador/notafiscal/App.java
+++ gerador-nota-fiscal/src/main/java/com/gerador/notafiscal/App.java
@@ -1,13 +1,32 @@
 package com.gerador.notafiscal;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import com.gerador.notafiscal.controllers.NotaFiscalController;
+
+import java.util.Scanner;
+
+public class App {
+    public static Scanner sc = new Scanner(System.in);
+    public static NotaFiscalController controller = new NotaFiscalController();
+
+    public static String input(String message) {
+        String result = "";
+        while (result.length() == 0) {
+            System.out.print(message);
+            result = sc.nextLine();
+        }
+
+        return result;
+    }
+
+    public static void main( String[] args ) {
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=");
+        System.out.println("Gerador de Nota Fiscal");
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=\n");
+
+        String clientName = input("Digite o nome do cliente: ");
+        String clientAddress = input("Digite o endereço do cliente: ");
+
+        String showOptions = "Qual é o tipo de serviço?\n";
     }
 }
