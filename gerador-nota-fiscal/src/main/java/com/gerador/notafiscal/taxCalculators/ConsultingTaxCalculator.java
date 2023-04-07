e7b5a924ce777edf2aeb96c6ab9ea2ff25682d8d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-06 23:37:20 -0300

    feat: add expection throw to consulting tax calculator

--- gerador-nota-fiscal/src/main/java/com/gerador/notafiscal/taxCalculators/ConsultingTaxCalculator.java
+++ gerador-nota-fiscal/src/main/java/com/gerador/notafiscal/taxCalculators/ConsultingTaxCalculator.java
@@ -3,6 +3,9 @@ package com.gerador.notafiscal.taxCalculators;
 public class ConsultingTaxCalculator implements TaxCalculator {
     @Override
     public double calculate(double value) {
+        if (value < 0)
+            throw new IllegalArgumentException("Value to tax must be equal or greater than zero");
+
         return value * 0.25;
     }
 }
