46b56b97bdcdafe4bfde286b54fdfb08888782a9
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-07 18:14:50 -0300

    feat: add dao sysout println tests

--- gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/services/NotaFiscalDaoTest.java
+++ gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/services/NotaFiscalDaoTest.java
@@ -3,11 +3,25 @@ package com.gerador.notafiscal.services;
 import com.gerador.notafiscal.models.Bill;
 import com.gerador.notafiscal.models.NotaFiscal;
 import com.gerador.notafiscal.models.enums.ServiceType;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
 public class NotaFiscalDaoTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
     public NotaFiscal notaFiscal;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
 
     @Before
     public void setup() {
@@ -15,9 +29,16 @@ public class NotaFiscalDaoTest {
         this.notaFiscal = new NotaFiscal(bill);
     }
 
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Test
     public void shouldHaveNoErrorsOnSave() {
         NotaFiscalDao notaFiscalDao = new NotaFiscalDao();
         notaFiscalDao.saveToDB(notaFiscal);
+
+        assertEquals("Salvando nota fiscal do cliente Davi Sousa no banco de dados...\n", outContent.toString());
     }
 }
