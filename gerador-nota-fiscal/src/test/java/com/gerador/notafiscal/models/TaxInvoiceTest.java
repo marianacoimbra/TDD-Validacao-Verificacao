f1359323aa1091b91e62adf085daf17cd0a62441
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-07 16:29:19 -0300

    refactor: add parameters to tax invoice test

--- gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/models/TaxInvoiceTest.java
+++ gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/models/TaxInvoiceTest.java
@@ -1,12 +1,41 @@
 package com.gerador.notafiscal.models;
 
 import com.gerador.notafiscal.models.enums.ServiceType;
+import com.gerador.notafiscal.taxCalculators.ConsultingTaxCalculator;
+import com.gerador.notafiscal.taxCalculators.DefaultTaxCalculator;
+import com.gerador.notafiscal.taxCalculators.TaxCalculator;
+import com.gerador.notafiscal.taxCalculators.TrainingTaxCalculator;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TaxInvoiceTest {
     public Invoice defaultInvoice;
     public TaxInvoice defaultTaxInvoice;
+
+    @Parameterized.Parameter
+    public ServiceType serviceType;
+
+    @Parameterized.Parameter(1)
+    public double initialValue;
+
+    @Parameterized.Parameter(2)
+    public double expectedTax;
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                // Run tests for each service type and its expected taxes
+                { ServiceType.CONSULTING, 1000.0, 250.0 },
+                { ServiceType.TRAINING, 1000.0, 150.0 },
+                { ServiceType.OTHER, 1000.0, 60.0 }
+        });
+    }
 
     @Before
     public void setup() {
@@ -15,20 +44,8 @@ public class TaxInvoiceTest {
     }
 
     @Test
-    public void shouldCreateConsultingTaxInvoice() {
-        Invoice invoice = new Invoice("João Alves", "Rua dos Bobos, 0", ServiceType.CONSULTING, 341.33);
-        new TaxInvoice(invoice);
-    }
-
-    @Test
-    public void shouldCreateTrainingTaxInvoice() {
-        Invoice invoice = new Invoice("João Alves", "Rua dos Bobos, 0", ServiceType.TRAINING, 341.33);
-        new TaxInvoice(invoice);
-    }
-
-    @Test
-    public void shouldCreateOtherTaxInvoice() {
-        Invoice invoice = new Invoice("João Alves", "Rua dos Bobos, 0", ServiceType.OTHER, 341.33);
+    public void shouldCreateTaxInvoice() {
+        Invoice invoice = new Invoice("João Alves", "Rua dos Bobos, 0", serviceType, initialValue);
         new TaxInvoice(invoice);
     }
 }
