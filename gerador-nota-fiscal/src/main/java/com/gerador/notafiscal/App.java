776c483a895dad19bbf92cfd1b1eafac80758ff8
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-07 20:17:43 -0300

    feat: add interactions to get service type

--- gerador-nota-fiscal/src/main/java/com/gerador/notafiscal/App.java
+++ gerador-nota-fiscal/src/main/java/com/gerador/notafiscal/App.java
@@ -2,7 +2,9 @@ package com.gerador.notafiscal;
 
 
 import com.gerador.notafiscal.controllers.NotaFiscalController;
+import com.gerador.notafiscal.models.enums.ServiceType;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -27,6 +29,28 @@ public class App {
         String clientName = input("Digite o nome do cliente: ");
         String clientAddress = input("Digite o endereço do cliente: ");
 
-        String showOptions = "Qual é o tipo de serviço?\n";
+        String serviceTypeDescription = getServiceTypeDescription();
+    }
+
+    public static String getServiceTypeDescription() {
+        List<String> serviceTypeDescriptions = ServiceType.getDescriptions();
+
+        StringBuilder showOptions = new StringBuilder("Qual é o tipo de serviço?\n");
+        for (int i = 0; i < serviceTypeDescriptions.size(); i++) {
+            int position = i + 1;
+            showOptions.append(position)
+                    .append(" - ")
+                    .append(serviceTypeDescriptions.get(i))
+                    .append("\n");
+        }
+        System.out.println(showOptions);
+
+        String choosedOption = input("Digite apenas o número do tipo: ");
+        try {
+            int index = Integer.parseInt(choosedOption) - 1;
+            return serviceTypeDescriptions.get(index);
+        } catch (Exception e) {
+            return getServiceTypeDescription();
+        }
     }
 }
