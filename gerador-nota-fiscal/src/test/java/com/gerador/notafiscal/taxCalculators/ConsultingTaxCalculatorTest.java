4c06000955abaf287b0d6477c384a3ffe05bec05
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2023-04-06 23:15:35 -0300

    feat: add trivial test to consulting tax calculator

---
+++ gerador-nota-fiscal/src/test/java/com/gerador/notafiscal/taxCalculators/ConsultingTaxCalculatorTest.java
@@ -0,0 +1,17 @@
+package com.gerador.notafiscal.taxCalculators;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ConsultingTaxCalculatorTest {
+
+    static public double maxDelta = 0.0001;
+
+    @Test
+    public void shouldCalculateTax() {
+        ConsultingTaxCalculator consultingTaxCalculator = new ConsultingTaxCalculator();
+        double tax = consultingTaxCalculator.calculate(1000.0);
+        assertEquals(100.0, tax, maxDelta);
+    }
+}
